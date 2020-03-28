0f6996d3381a7ebde01a52874ea566921ad6168d
Luiz Henrique <luiz.pventura@gmail.com>
2020-03-28 10:54:42 -0300

    back end da aplicação

--- aulas/backend/knexfile.js
+++ aulas/backend/knexfile.js
@@ -2,47 +2,47 @@
 
 module.exports = {
 
-    development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './src/database/db.sqlite'
-        },
-        migrations: {
-            directory: './src/database/migrations'
-        },
-        useNullAsDefault: true,
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
     },
-
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+    migrations: {
+      directory: './src/database/migrations'
     },
+    useNullAsDefault: true,
+  },
 
-    production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
-const SerrionController = require('./controllers/SerrionController')
+const SessionController = require('./controllers/SessionController')
 
-const routes = express.Router();
+const routes = express.Router()
 
-routes.post('/session', SerrionController.create)
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', OngController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.get('/profiles', ProfileController.index)
 
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
+
 routes.delete('/incidents/:id', IncidentController.delete)
 
-module.exports = routes;
+module.exports = routes
